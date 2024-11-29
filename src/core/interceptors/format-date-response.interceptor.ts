c2ee396fd70ca6e4a39db3083d08bafb7ec1fd45
iamhafa <iamhafaa@gmail.com>
2024-11-29 15:45:03 +0700

    add lodash

--- src/core/interceptors/format-date-response.interceptor.ts
+++ src/core/interceptors/format-date-response.interceptor.ts
@@ -1,6 +1,7 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { format } from 'date-fns';
+import { isNull, isObject } from 'lodash';
 
 @Injectable()
 export class FormatDateResponseInterceptor implements NestInterceptor {
@@ -16,13 +17,13 @@ export class FormatDateResponseInterceptor implements NestInterceptor {
     if (Array.isArray(response)) {
       // continue format date
       return response.map((item) => this.formatDates(item));
-    } else if (response !== null && typeof response === 'object') {
+    } else if (!isNull(response) && isObject(response)) {
       Object.keys(response).forEach((key: string) => {
         // nếu property nào của 1 object thuộc kiểu Date
         if (response[key] instanceof Date) {
           response[key] = format(response[key], 'dd-MM-yyyy HH:mm'); // Desired format
           // nếu property vẫn là object thì tiếp tục run format
-        } else if (typeof response[key] === 'object') {
+        } else if (isObject(response[key])) {
           response[key] = this.formatDates(response[key]);
         }
       });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { swaggerConfig } from '@/config/swagger.config';
 import { FormatDateResponseInterceptor } from '@/core/interceptors/format-date-response.interceptor';
@@ -8,6 +9,7 @@ import { HttpExceptionFilter } from '@/core/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
   app.enableCors();
 
   // Enable for validation (when use DTO)
@@ -17,14 +19,14 @@ async function bootstrap() {
   // Filter to format error response
   app.useGlobalFilters(new HttpExceptionFilter());
   // Prefix
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(configService.get<string>('APP_PREFIX_API'));
 
   const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, documentFactory);
+  SwaggerModule.setup(configService.get<string>('APP_SWAGGER_PATH'), app, documentFactory);
 
   await app.listen(process.env.APP_PORT, () => {
     const logger: Logger = new Logger('Bootstrap');
-    logger.verbose(`The app running on http://localhost:${process.env.APP_PORT}`);
+    logger.verbose(`The app running on http://localhost:${configService.get<string>('APP_PORT')}`);
   });
 }
 bootstrap();
